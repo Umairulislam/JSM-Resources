4b8292d4c94ff568df37f8affe0de7fdbba29d0e
Umairulislam <umairulislam09@gmail.com>
2023-10-03 22:59:48 +0500

    quotation fixed

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,14 +4,14 @@ const Header = ({ query, category }) => {
   if (query && category) {
     return (
       <h1 className="heading3 self-start text-white-800">
-        Search results for "{query}" in <span className="capitalize">{category}</span>
+        Search results for {query} in <span className="capitalize">{category}</span>
       </h1>
     );
   }
   if (query) {
     return (
       <h1 className="heading3 self-start text-white-800">
-        Search results for "{query}"
+        Search results for {query}
       </h1>
     );
   }
